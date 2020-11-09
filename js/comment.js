b32e48407c1c803e02fe16d9ee97f2521460d53c
Abinash Thapa <abinashthapa@Abinashs-MacBook-Pro.local>
2020-11-09 12:04:38 -0600

    Add sth feature

---
+++ js/comment.js
@@ -0,0 +1,23 @@
+function buildComments(){
+    var commentList = [ {
+name:  "abinash", 
+date: "1st jan",
+comment: "hello"} , {
+name:"abinash",
+date: "1st jan",
+comment: "hello"
+} ];
+  console.log("test");
+    var div = document.getElementById("comms"); /* langdiv replace by comment division */
+
+    var html = "";
+    for (var i=0;i<commentList.length;i++) {
+        html += `<div class="media-object pull-left"> <img src="images/avatar.png" class="img-responsive img-circle" alt="Blog Image 11"></div><div class="media-body"> <h3 class="media-heading"> ${commentList[i].name}  </h3><span> ${commentList[i].date}  </span>
+                                             <p>${commentList[i].comment}  </p> </div>`
+    }
+    
+
+    div.innerHTML = html;
+}
+
+buildComments()
